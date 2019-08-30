6bb6549468477d174abbd3904506e73f81ecdf80
Mohamad Qaddura <msqaddura@gmail.com>
2019-08-30 19:03:45 +0300

    connect movie domain

--- src/app/movie/movie.module.ts
+++ src/app/movie/movie.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MovieModule } from '../movie/movie.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
-    MovieModule
+    CommonModule
   ]
 })
-export class DomainsModule { }
+export class MovieModule { }
