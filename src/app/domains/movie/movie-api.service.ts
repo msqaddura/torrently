84391554cc44788781aab2c07febdcd8848b925b
Mohamad Qaddura <msqaddura@gmail.com>
2019-08-30 19:54:57 +0300

    finalize the workflow

--- src/app/domains/movie/movie-api.service.ts
+++ src/app/domains/movie/movie-api.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+import { movies } from './movie.mock-data';
+import { GenreType } from './movie.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieApiService {
+  constructor() {}
+  getMovieList() {
+    return of(movies);
+  }
 
-  constructor() { }
+  // Imagine we have database of movies, we rather than query the movies by filter, 
+  getMovieListByGenre(genreType: GenreType) {
+    return of(movies).pipe(
+      map(movieList =>
+        movieList.filter(movie =>
+          movie.genres.findIndex(genre => genre === genreType)
+        )
+      )
+    );
+  }
 }
